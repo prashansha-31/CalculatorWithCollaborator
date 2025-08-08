f5d8057749c4fb719e11806c886e507b3dd21343
Prashansha Maheshwari <prashansha.maheshwari_cs.aim24@gla.ac.in>
2025-08-08 11:37:24 +0530

    [Prashansha Maheshwari] added add java file

---
+++ add.java
@@ -0,0 +1,12 @@
+ class Add {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        Add calculator = new Add();
+        int result = calculator.add(a:5, b:3);
+        System.out.printl("The sum is:" + result);
+    }
+ }
+    
+
